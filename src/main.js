234ca62262be80088ff80e89df2b8682d4843a9d
chrisangelos772 <c.angelos772@gmail.com>
2020-11-17 23:02:26 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -6,5 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["my", "the", "our", "their"];
+  var adj = ["huge", "small", "clean", "costly"];
+  var noun = ["pillow", "house", "car", "swamp"];
+
+  var domain = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let newDomain = pronoun[i] + adj[j] + noun[k] + ".com";
+        domain.push(newDomain);
+      }
+    }
+  }
+  //console.log(domain);
+  let ul = document.querySelector("#domain-list");
+  ul.innerHTML = buildlist(domain);
 };
+
+function buildlist(list) {
+  return list
+    .map(item => {
+      return "<li class='mb-2'>" + item + "</li>";
+    })
+    .join("");
+}
